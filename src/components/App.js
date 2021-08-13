48fe6590b499ee32cd3fcfb97226dc9f6d706724
Олеся Сороко <oleestra@MacBook-Air-Olesa-2.local>
2021-08-13 23:42:44 +0300

    problem

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import Main from "./Main";
 import Footer from "../components/Footer";
 import ImagePopup from "./ImagePopup";
-import { api } from "../utils/Api";
+import { api } from "../utils/api";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
